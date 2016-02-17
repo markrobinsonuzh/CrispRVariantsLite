10c417e16c2ccfce09ffaab753583cee14e835b1
smrtnrd <berthin.biyong@students.unibe.ch>
2016-02-17 18:25:38 +0100

    add setting for genome repo

--- core/setting.R
+++ core/setting.R
@@ -3,13 +3,11 @@
 # genome <- "/home/Shared_taupo/data/annotation/Danio_rerio/genome_danRer7"
 genome <- "./data/genome"
 
-gendb <- dir(genome, pattern = ".fa$", recursive = TRUE, full.names = TRUE)
-gendb.gz <- dir(genome, recursive = TRUE, full.names = TRUE)
-
+gendb <- dir(genome, recursive = TRUE, full.names = TRUE)
 
 # name of sequence
 genlist <- basename(gendb)
-genlist.gz <- basename(gendb.gz)
+genlist.gz <- basename(gendb)
 
 # transcript database library
 txDB.dir <- dir("./data/txdb/", recursive = TRUE, full.names = TRUE)
