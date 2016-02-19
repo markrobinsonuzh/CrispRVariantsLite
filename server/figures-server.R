428e661123f7c08c11f8d82af1ab3275f8fff8f2
smrtnrd <berthin.biyong@students.unibe.ch>
2016-02-19 01:51:03 +0100

    fix location

--- server/figures-server.R
+++ server/figures-server.R
@@ -14,7 +14,7 @@ output$plots <- renderUI({
 
 # Disable running of the plots until Reference and Bams defined
 observe({
-   if(is.null(d$ref)){
+   if(is.null(d$guide)){
       updateButton(session, "run_plot", style ="default", icon = icon("ban"), disable = TRUE )
     }else{
       updateButton(session,"run_plot", 'Plot', icon =  icon("area-chart"), style = "success", block = TRUE, disable = FALSE ) 
@@ -52,7 +52,7 @@ createCripSet <- reactive({
     }
     print(d$guide)
     
-    d$cset <- readsToTarget(v$bm_fnames, target = d$guide, reference = d$ref, names = md$label, target.loc = input$target_loc)
+    d$cset <- readsToTarget(v$bm_fnames, target = d$guide, reference = setRef(), names = md$label, target.loc = input$target_loc)
     
     print(d$cset)
 
@@ -85,32 +85,41 @@ createCrispPlot <- reactive({
      
      try({
       plotVariants(d$cset, txdb = d$txdb, 
-      col.ht.ratio = c(1,6),
-      left.plot.margin = grid::unit(c(0.1,0,6,0.2), "lines"),
+      col.with.ratio = c(4,2),
+      row.ht.ratio = c(1,8),
+      gene.text.size = 8,
+      left.plot.margin = ggplot2::unit(c(1,0,5,2), "lines"),
       
+      plotAlignments.args = list(
+        legend.cols =2,
+        top.n = input$top.n,
+        min.freq = input$min.freq,
+        min.count = input$min.count,
+        target.loc = d$t.loc,
+        guide.loc = IRanges(
+         start = d$seq.width + 1,
+         end = end(d$guide) - start(d$guide) - d$seq.width + 1),
+        axis.text.size = input$axis.text.size, 
+        ins.size = input$ins.size,
+        plot.text.size = input$plot.text.size, 
+        legend.symbol.size = input$legend.symbol.size, 
+        legend.text.size = input$legend.text.size
+        ), 
+        
       plotFreqHeatmap.args = list(
         top.n = input$top.n,
         min.freq = input$min.freq,
         min.count = input$min.count,
+        type =  input$plot.type,
         x.size = input$x.size, 
         plot.text.size = input$plot.text.size, 
-        legend.text.size = input$legend.text.size, 
+        legend.text.size = input$legend.text.size,
+        legend.key.height = grid::unit(2, "lines"), 
         x.angle = input$x.angle
-        ),
-      
-      plotAlignments.args = list(
-        top.n = input$top.n,
-        min.freq = input$min.freq,
-        min.count = input$min.count,
-        axis.text.size = input$axis.text.size, 
-        ins.size = input$ins.size, 
-        legend.symbol.size = input$legend.symbol.size, 
-        legend.text.size = input$legend.text.size
-        ), 
-        row.ht.ratio = c(1,6)
+        )
       )  
      }, silent = TRUE)
-  })
+  }, height = 600)
   return(pcrisp)
 })
 
